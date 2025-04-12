14dc423cf03a50bb283c389d9b20ad836fd2968d
Lorenzo Gandino <lorenzoga94@gmail.com>
2025-04-12 20:43:00 +0200

    Add table output

--- main/ls-command.go
+++ main/ls-command.go
@@ -6,14 +6,21 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"slices"
 	"strings"
 )
 
+var validOutputFlags = []string{
+	"table",
+}
+
+type NotePrintFunc = func(Note)
 type LsConf struct {
 	XTitle string
 	XId string
 	Tags []string
 	TagsOr []string
+	Output string
 }
 
 func LsCommand(conf AppConfig, args []string) {
@@ -31,6 +38,7 @@ func LsCommand(conf AppConfig, args []string) {
 	if err != nil {
 		log.Fatalf("main: Unable to read notes files %v", err)
 	}
+	notePrintFunc := NotePrint(lsConf.Output)
 	for _, file := range files {
 		notes, err := GetNoteData(file)
 		if err != nil {
@@ -46,7 +54,7 @@ func LsCommand(conf AppConfig, args []string) {
 			if !rxTitle.MatchString(note.Title) {
 				continue
 			}
-			fmt.Printf("%s: %s\n", note.Id, note.Path)
+			notePrintFunc(note)
 		}
 	}
 }
@@ -56,6 +64,7 @@ func LsFlags(args []string) LsConf {
 	lsf := flag.NewFlagSet("ls", 0)
 	lsf.StringVar(&conf.XId, "xid", ".", "Regex match id")
 	lsf.StringVar(&conf.XTitle, "xtitle", ".", "Regex match title")
+	lsf.StringVar(&conf.Output, "o", "table", "Output format. [table]")
 	tags := lsf.String("t", "", "Tags AND")
 	tagsOr := lsf.String("tor", "", "Tags OR")
 	err := lsf.Parse(args)
@@ -71,5 +80,43 @@ func LsFlags(args []string) LsConf {
 		conf.TagsOr = strings.Split(*tagsOr, ",")
 	}
 
+	if !slices.Contains(validOutputFlags, conf.Output) {
+		log.Fatalf("Invalid output flag %s", conf.Output)
+	}
 	return conf
+}
+
+func NotePrint(outputType string) NotePrintFunc {
+	switch outputType {
+	default:
+		return TablePrintNote()
+	}
+}
+
+func TablePrintNote() NotePrintFunc {
+	const (
+		idWidth    = 24
+		titleWidth = 30
+		tagsWidth  = 30
+	)
+
+	headerFmt := fmt.Sprintf("%%-%ds  %%-%ds  %%-%ds  %%s\n",
+	idWidth, titleWidth, tagsWidth)
+	rowFmt := headerFmt
+
+	fmt.Printf(headerFmt, "ID", "TITLE", "TAGS", "PATH")
+	fmt.Println(strings.Repeat("-", idWidth+titleWidth+tagsWidth+10) + "----------------------------------------")
+
+	return func(n Note) {
+		title := truncate(n.Title, titleWidth)
+		tags := truncate(strings.Join(n.Tags, ", "), tagsWidth)
+		fmt.Printf(rowFmt, n.Id, title, tags, n.Path)
+	}
+}
+
+func truncate(s string, max int) string {
+	if len(s) <= max {
+		return s
+	}
+	return s[:max-3] + "..."
 }
