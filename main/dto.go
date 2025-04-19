e191a2a81df1821b447c61a5f434e7836eaa2c97
Lorenzo Gandino <lorenzoga94@gmail.com>
2025-04-19 21:25:55 +0200

    Add missing title

--- main/dto.go
+++ main/dto.go
@@ -18,6 +18,7 @@ type Repository struct {
 // Define basic note operations
 type Note interface {
     Id() NoteId
+    Title() string
     Tags() []string
     Links() []Note
     LastUpdate() *time.Time
@@ -28,12 +29,17 @@ type Note interface {
 // Define basic data for notes operations
 type BaseNote struct {
 	id NoteId `yaml:"id"`
+	title NoteId `yaml:"title"`
 	tags []string `yaml:"tags"`
 	links []Note `yaml:"links"`
 }
 
 func (n *BaseNote) Id() NoteId {
 	return n.id
+}
+
+func (n *BaseNote) Title() NoteId {
+	return n.title
 }
 
 func (n *BaseNote) Tags() []string {
